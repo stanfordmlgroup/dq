64f3200c3f8fb8803e3f0125f479035fa9f1ffc2
Anand Avati <avati@cs.stanford.edu>
2016-05-11 13:40:40 -0700

    prune away setuid execution
    Signed-off-by: Anand Avati <avati@cs.stanford.edu>

--- install.sh
+++ install.sh
@@ -11,15 +11,6 @@ D=$(cd `dirname $0` && pwd)
 
 #DIR=$1
 
-function install_fix {
-    SRC=$1; shift
-    DST=$1; shift
-    echo "#!$DIR/shell -p" > $SRC.fix
-    tail -n +2 $SRC >> $SRC.fix
-    install $SRC.fix $DST $@
-    rm -f $SRC.fix
-}
-
 install $D/dq-on-all $DIR/dq-on-all -D -m 0755
 install $D/dq-submit $DIR/dq-submit -D -m 0755
 install $D/dq-prepare $DIR/dq-prepare -D -m 0755
@@ -42,10 +33,6 @@ install $D/dq-users $DIR/dq-users -D -m 0755
 install $D/dq-env.sh $DIR/dq-env.sh -D -m 0644
 install $D/dq-env.csh $DIR/dq-env.csh -D -m 0644
 install /bin/bash $DIR/shell -D -o root -g $GROUP -m 06550
-install /usr/bin/screen $DIR/screen -D -o root -g $GROUP -m 06750
 install $D/scripts/helper-on-all $DIR/scripts/helper-on-all -D -m 0755
-install_fix $D/scripts/gpu-users $DIR/scripts/gpu-users -D -m 0755
-install_fix $D/scripts/gpu-free $DIR/scripts/gpu-free -D -m 0755
-install_fix $D/scripts/gpu-free-list $DIR/scripts/gpu-free-list -D -m 0755
 
 chmod g+w $DIR
